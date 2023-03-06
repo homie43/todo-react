616c5212e2738de2d7e12a790aa4c278e17f9266
Vladimir <homie43@yandex.ru>
2023-03-06 18:52:42 +0300

    todo completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
+import React from "react";
+
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [todoList, setTodoList] = React.useState([]);
+  const [text, setText] = React.useState("");
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodoList([
+        ...todoList,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+      setText("");
+    }
+  };
+
+  const toggleTodoCompleted = (idx) => {
+    setTodoList(
+      todoList.map((item) => {
+        if (item.id !== idx) return todoList;
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      })
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodoList(todoList.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => toggleTodoCompleted(item.id)}
+            />
+            <span>{item.text}</span>
+            <span
+              onClick={() => removeTodo(item.id)}
+              style={{ color: "red", cursor: "pointer" }}
+            >
+              &times;
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
